3b984fa52d1015b2126f1aaffe5c67e2657ee3c4
serg shutin <saldius@gmail.com>
2023-03-27 23:35:19 +0900

    add sub_2_num() to funcs.py

--- funcs.py
+++ funcs.py
@@ -2,3 +2,9 @@ def sum_2_num():
     a = int(input())
     b = int(input())
     print(int(a) + int(b))
+
+    sum_2_num()
+def sub_2_num():
+    a = int(input())
+    b = int(input())
+    print(int(a) - int(b))
